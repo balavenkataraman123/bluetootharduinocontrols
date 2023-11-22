2342325b4d110e7cdd2a7f3338cbc32642080e5e
Bala Venkataraman <38150148+balavenkataraman123@users.noreply.github.com>
2023-11-22 10:01:08 -0500

    Create sketch.ino

---
+++ sketch.ino
@@ -0,0 +1,21 @@
+#define LED_PIN 13
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(LED_PIN, OUTPUT); // set the digital pin as output:
+}
+
+void loop() {
+  Serial.println("Send a command");
+  if (Serial.available()) { // if there is data comming
+    String command = Serial.readStringUntil('\n'); // read string until meet newline character
+
+    if (command == "OFF") {
+      digitalWrite(LED_PIN, LOW);  // turn off LED
+      Serial.println("LED is turned OFF"); // reports action to smartphone app
+    } else if (command == "ON") {
+      digitalWrite(LED_PIN, HIGH); // turn on LED
+      Serial.println("LED is turned ON"); // reports action to smartphone app
+    }
+  }
+}
